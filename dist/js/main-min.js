344b6b95af5b49966d3ac6b9f2402471b782c647
Joseph Layden <layden.joseph@gmail.com>
2021-12-29 18:15:02 -0600

    adding sticky header on scroll

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-function getCopyrightYear(){document.getElementById("year").innerHTML=(new Date).getFullYear()}getCopyrightYear();
+function getCopyrightYear(){document.getElementById("year").innerHTML=(new Date).getFullYear()}getCopyrightYear();let header=document.getElementById("masthead"),sticky=header.offsetTop;function stickyHeader(){window.pageYOffset>sticky?header.classList.add("sticky"):header.classList.remove("sticky")}window.onscroll=function(){stickyHeader()()};
--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,20 @@ function getCopyrightYear() {
     document.getElementById("year").innerHTML = new Date().getFullYear();
 }
 getCopyrightYear();
+
+/**
+ * Sticky header
+ * Add the sticky class to the header when you reach its scroll position. 
+ * Remove "sticky" when you leave the scroll position
+ */
+let header = document.getElementById('masthead'); // Get the header
+let sticky = header.offsetTop; // Get the offset position of the navbar
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
+// When the user scrolls the page, add sticky header
+window.onscroll = function() {stickyHeader()()};
