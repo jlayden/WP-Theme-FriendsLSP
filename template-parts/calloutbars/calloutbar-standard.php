0335b76538c063362fd99e3d513adeaa4465c8c4
Joseph Layden <layden.joseph@gmail.com>
2022-05-20 12:40:56 -0500

    adding ACF for standard calloutbar

--- template-parts/calloutbars/calloutbar-standard.php
+++ template-parts/calloutbars/calloutbar-standard.php
@@ -1,23 +1,54 @@
-<?php $display = get_field('callout_section_display'); // checkbox ?>
-<?php $background_color = get_field('callout_section_background_color'); // choice text ?>
-<?php $css_class = get_field('callout_section_css_class'); // choice text ?>
-<?php $subtitle = get_field('callout_section_subtitle'); // text ?>
-<?php $title = get_field('callout_section_title'); // text ?>
-<?php $button = get_field('callout_section_button'); // group ?>
-<?php if ($display == 'visible') { ?>	
-<section class="calloutbar <?php echo $css_class; ?> <?php echo $background_color; ?>">
+<?php 
+$display = get_field('callout_section_display'); // checkbox 
+$content = get_field('callout_section_overlay_content'); // wysiwig 
+$background_image = get_field('callout_section_background_image'); // image ID 
+$background_color = get_field('callout_section_background_color'); // image ID 
+$featured_image = get_field('callout_section_featured_image'); // image ID 
+$link = get_field('callout_section_button');
+if( $link ): 
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+endif;
+$background_size = "full"; // (thumbnail, medium, large, full or custom size)
+$background_img = wp_get_attachment_image_src($background_image, $background_size);
+$featured_size = "full"; // (thumbnail, medium, large, full or custom size)
+$featured_img = wp_get_attachment_image_src($featured_image, $featured_size);
+
+if ($display !== 'hidden' && $display !== 'standard' && $display !== 'reversed') { ?>
+<section class="calloutbar <?php echo $display ?> <?php echo $background_color ?>" style="background-image: url('<?php echo $background_img[0]; ?>');">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
-                <h5><?php echo $subtitle; ?></h5>
-                <h2><?php echo $title; ?></h2>
-            </div>
-            <div class="col-lg-6">
-                <div class="cta">
-                    <a href="<?php echo $button['link']; ?>" class="btn btn-primary"><?php echo $button['text']; ?></a>
+            <div class="content-wrapper offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 offset-md-1 col-md-10 col-sm-12 in-bottom">
+                <?php if ($featured_image) { ?>
+                    <img src="<?php echo $featured_img[0]; ?>" class="d-block mx-lg-auto img-fluid img-overlay" alt="overlay image" loading="lazy">
+                <?php } ?>
+                <div class="content">
+                    <?php echo $content; ?>
                 </div>
+                <?php if ($link) { ?>
+                    <div class="cta">
+                        <a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                    </div>
+                <?php } ?>
             </div>
         </div>
+    </div>
+</section>
+<?php } else { ?>
+<section class="calloutbar <?php echo $background_color ?> <?php echo $display ?>" style="background-image: url('<?php echo $background_img[0]; ?>');">
+    <?php if ($featured_image) { ?>
+    <div class="image-wrapper" style="background-image: url('<?php echo $featured_img[0]; ?>');">
+        <img src="<?php echo $featured_img[0]; ?>" class="d-block mx-lg-auto img-fluid img-overlay" alt="overlay image" loading="lazy">
+    </div>
+    <?php } ?>
+    <div class="content-wrapper">
+        <?php echo $content; ?>
+        <?php if ($link) { ?>
+            <div class="cta">
+                <a class="btn btn-primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+            </div>
+        <?php } ?>
     </div>
 </section>
 <?php } ?>
--- template-parts/sections/section-richtext.php
+++ template-parts/sections/section-richtext.php
@@ -1,9 +1,16 @@
-<?php $richtext = get_field('richtext'); // WYSIWIG rich text editor ?>
+<?php $richtext = get_field('richtext'); // WYSIWIG rich text editor 
+
+if ($richtext){?>
+
 <section class="richtext">
     <div class="container">
+        <div class="row">
             <div class="col-lg-12">
                 <?php echo $richtext; ?>
             </div>
-		</div>
+            </div>
+        </div>
     </div>
 </section>
+
+<?php } ?>
